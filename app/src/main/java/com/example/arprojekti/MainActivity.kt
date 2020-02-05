db2dbaadfbfa763af244425bff9b8e5e6ea8ecf7
Oona <oona.e.rauhala@gmail.com>
2020-02-05 14:09:44 +0200

    Add button and working 3d model

--- app/src/main/java/com/example/arprojekti/MainActivity.kt
+++ app/src/main/java/com/example/arprojekti/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.arprojekti
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import com.google.ar.core.HitResult
 import com.google.ar.core.Plane
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.HitTestResult
+import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.rendering.ViewRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
@@ -15,33 +19,49 @@ import com.google.ar.sceneform.ux.TransformableNode
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fragment: ArFragment
-    private var testRenderable: ViewRenderable? = null
+    private var testRenderable: ModelRenderable? = null
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment;
-        val renderableFuture = ViewRenderable.builder()
-            .setView(this, R.layout.rendtext)
+
+        //get fragment & button
+        fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener{ view -> addObject() }
+        fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as
+                ArFragment
+
+        val modelUri = Uri.parse("valmisleipa3.sfb")
+        val renderableFuture = ModelRenderable.builder()
+            .setSource(fragment.context, modelUri)
             .build()
         renderableFuture.thenAccept { it -> testRenderable = it }
-
-        fragment.setOnTapArPlaneListener { hitResult: HitResult?, plane: Plane?, motionEvent: MotionEvent? ->
-            if (testRenderable == null) {
-                return@setOnTapArPlaneListener
-            }
-            val anchor = hitResult!!.createAnchor()
-            val anchorNode = AnchorNode(anchor)
-            anchorNode.setParent(fragment.arSceneView.scene)
-            val viewNode = TransformableNode(fragment.transformationSystem)
-            viewNode.setParent(anchorNode)
-            viewNode.renderable = testRenderable
-            viewNode.select()
-            viewNode.setOnTapListener { hitTestRes: HitTestResult?, motionEv: MotionEvent? ->
-                Toast.makeText(applicationContext, "Ouch!!!!", Toast.LENGTH_SHORT).show()
-            }
-
-        }
     }
 
+    private fun addObject() {
+        val frame = fragment.arSceneView.arFrame
+        val pt = getScreenCenter()
+        val hits: List<HitResult>
+        if (frame != null && testRenderable != null) {
+            hits = frame.hitTest(pt.x.toFloat(), pt.y.toFloat())
+            for (hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane) {
+                    val anchor = hit!!.createAnchor()
+                    val anchorNode = AnchorNode(anchor)
+                    anchorNode.setParent(fragment.arSceneView.scene)
+                    val mNode = TransformableNode(fragment.transformationSystem)
+                    mNode.setParent(anchorNode)
+                    mNode.renderable = testRenderable
+                    mNode.select()
+                    break
+                } } } }
+
+    private fun getScreenCenter(): android.graphics.Point {
+        val vw = findViewById<View>(android.R.id.content)
+        return android.graphics.Point(vw.width / 2, vw.height / 2)
+    }
 }
